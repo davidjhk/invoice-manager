2710737518c05021b2a2e3e90c91c5013f875d0d
JDOSA <davidjhk@gmail.com>
2025-07-11 05:37:08 -0700

    Fix update

--- web/index.php
+++ web/index.php
@@ -1,13 +1,8 @@
 <?php
 
-// Force debug mode for troubleshooting
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+// defined('YII_DEBUG') or define('YII_DEBUG', true);
+// defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
