44e22a4f51bc9e03c507848ac0639b9873c5b8c2
Debian <bitnami@ip-172-31-15-101.us-west-1.compute.internal>
2025-07-27 20:43:21 +0000

    security delete

--- fix_estimate_user_id.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Fix estimate user_id column issue
-# This script should be run on the server where the database is accessible
-
-echo "Running migration to add user_id column to estimates table..."
-./yii migrate --interactive=0
-
-echo "Migration completed. The estimate creation issue should now be resolved."
